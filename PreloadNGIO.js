96dc50c853670112fa23dfa22b77d05b7e8af876
Elijah Wiencek <burningmagma@protonmail.com>
2026-05-30 21:03:31 -0700

    Add files via upload

--- PreloadNGIO.js
+++ PreloadNGIO.js
@@ -4,6 +4,13 @@ setTimeout(function()
 {
     let ngioInterval = setInterval(function()
     {
+        // Wait for InitNGIO.js to finish setting globalThis.NGIO_API
+        if (!globalThis.NGIO_API)
+        {
+            console.log("Waiting for NGIO_API...");
+            return;
+        }
+
         globalThis.NGIO_API.getConnectionStatus(function(status)
         {
             console.log("NGIO Status:", status);
@@ -16,13 +23,14 @@ setTimeout(function()
                         ngioReady = true;
                         clearInterval(ngioInterval);
 
-                        globalThis.NGIO_API.getMedal(89935);
-                        globalThis.NGIO_API.getMedal(90406);
-                        globalThis.NGIO_API.getMedal(90407);
-                        globalThis.NGIO_API.getMedal(90408);
+                        globalThis.NGIO_API.unlockMedal(89935);
+                        globalThis.NGIO_API.unlockMedal(90406);
+                        globalThis.NGIO_API.unlockMedal(90407);
+                        globalThis.NGIO_API.unlockMedal(90408);
+                        console.log("Medals unlocked!");
                     }
                 break;
             }
         });
     }, 16);
-}, 1000);
+}, 500);
