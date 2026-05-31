f72f560ad1e1bc25907926cd2b4a56c6b55f6de1
Elijah Wiencek <burningmagma@protonmail.com>
2026-05-30 21:45:00 -0700

    Add files via upload

--- PreloadNGIO.js
+++ PreloadNGIO.js
@@ -29,10 +29,11 @@ setTimeout(function()
                             }
                         }
                         setInterval(function()
-                        {
-                            globalThis.NGIO_API.getConnectionStatus(function(status) {});
-                            globalThis.NGIO_API.keepSessionAlive();
-                        }, 1000);
+						{
+							globalThis.NGIO_API.getConnectionStatus(function(status) {});
+							if (globalThis.NGIO_API.hasUser)
+								globalThis.NGIO_API.keepSessionAlive();
+						}, 1000);
                     }
                 break;
             }
